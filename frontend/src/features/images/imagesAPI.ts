4a3844e79366a90bab5e4b875b2f661dffa7e67e
EyalKoubi <109479738+EyalKoubi@users.noreply.github.com>
2025-05-12 13:42:28 +0300

    Update imagesAPI.ts (Update server url)

--- frontend/src/features/images/imagesAPI.ts
+++ frontend/src/features/images/imagesAPI.ts
@@ -5,7 +5,7 @@ export const fetchPhotos = async (
   page: number,
   perPage: number = 9
 ) => {
-  const res = await axios.get("http://localhost:3001/api/photos", {
+  const res = await axios.get("https://msappshomework.onrender.com/api/photos", {
     params: { category, page, perPage },
   });
   return res.data;
